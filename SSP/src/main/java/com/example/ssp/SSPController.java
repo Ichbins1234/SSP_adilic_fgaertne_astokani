bc059011b954012bba8d2ba1e15f00a8a2aec3c6
fgaertne <fgaertne@htl-steyr.ac.at>
2022-11-28 11:29:07 +0100

    v5

--- SSP/src/main/java/com/example/ssp/SSPController.java
+++ SSP/src/main/java/com/example/ssp/SSPController.java
@@ -1,14 +1,20 @@
 package com.example.ssp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class SSPController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button paper;
+
+    @FXML
+    private Button scissor;
+
+    @FXML
+    private Button stone;
+
+
+
 }
