3fd34579b5cc2597b03f387374f12a2ae039868e
adilic <adilic@htl-steyr.ac.ar>
2022-11-29 14:36:55 +0100

    winner

--- SSP/src/main/java/com/example/ssp/SSPApplication.java
+++ SSP/src/main/java/com/example/ssp/SSPApplication.java
@@ -21,3 +21,4 @@ public class SSPApplication extends Application {
         launch();
     }
 }
+
--- SSP/src/main/java/com/example/ssp/SSPController.java
+++ SSP/src/main/java/com/example/ssp/SSPController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.util.Random;
+
 public class SSPController {
 
     @FXML
@@ -15,6 +17,50 @@ public class SSPController {
     @FXML
     private Button stone;
 
+    private int choice;
+
+    Random random = new Random();
+    int choice2 = random.nextInt();
+
+    private String player = "player";
+    private String ai = "AI";
+    private String winner = "";
+
+
+    @FXML
+    public void OnScissorsClick() {
+        choice = 1;
+    }
+
+    @FXML
+    public void OnStoneClick() {
+        choice = 2;
+    }
+
+    @FXML
+    public void OnPaperClick() {
+        choice = 3;
+    }
+    
+    @FXML
+    public void Winner(){
+        if (choice == 1 && choice2 == 3 ){
+           winner = player;
+        } else if (choice == 1 && choice2 == 2) {
+           winner = ai;
+        } else if (choice == 2 && choice2 == 1) {
+            winner = player;
+        } else if (choice == 2 && choice2 == 3) {
+            winner = ai;
+        } else if (choice == 3 && choice2 == 1) {
+            winner = ai;
+        } else if (choice == 3 && choice2 == 2) {
+            winner = player;
+        }
+    }
+    
+
 
 
 }
+
